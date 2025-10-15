0ef16451c2b6b969332f046871d1a82429536ea1
Michael Boateng <michaelboateng064@gmail.com>
2025-10-15 09:10:02 +0000

    Added NavCards below hero

---
+++ src/routes/components/HeroNav.svelte
@@ -0,0 +1,30 @@
+<script>
+	import HeroNavCard from './HeroNavCard.svelte';
+	import ShapeDesign from './shapeDesign.svelte';
+
+	const cardList = [
+		{
+			title: 'Academics',
+			icon: 'fa-solid fa-book pt-10 text-7xl text-[#04095d]',
+		},
+		{
+			title: 'Academics',
+			icon: 'fa-solid fa-book pt-10 text-7xl text-[#04095d]',
+		},
+		{
+			title: 'Academics',
+			icon: 'fa-solid fa-book pt-10 text-7xl text-[#04095d]',
+		}
+	];
+</script>
+
+
+<div class=" flex h-[200px] h-[400px] items-center justify-evenly py-20">
+	{#each cardList as card, i}
+	<HeroNavCard {i}>
+		<i class={card.icon}></i>
+		<p class="py-10 text-3xl font-semibold text-[#04095d]">{card.title}</p>
+	</HeroNavCard>
+	{/each}
+</div>
+<ShapeDesign />
