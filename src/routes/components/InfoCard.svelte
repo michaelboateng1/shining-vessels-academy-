ba3b41d6119381dc2250eb1015f2e5a16e8acf58
Michael Boateng <michaelboateng064@gmail.com>
2025-10-21 08:07:13 +0000

    Added aso animation to the about page

--- src/routes/components/InfoCard.svelte
+++ src/routes/components/InfoCard.svelte
@@ -6,9 +6,9 @@
 
 </script>
 
-<section class="flex items-center justify-between {flip ? 'flex-row-reverse bg-[#e9ecef]' : 'flex-row'}">
+<section class="flex items-center mt-30 justify-between {flip ? 'flex-row-reverse bg-[#e9ecef]' : 'flex-row'}">
     <div class="{flip ? 'border-l-2' : 'border-r-2 rounded-full'} border-[#e9ecef] pl-5 h-[500px] flex flex-col justify-center items-center px-20">
-        <div class="p-10">
+        <div class="p-10" data-aos="zoom-in" data-aos-delay={150}>
             <h2 class="mb-5 text-5xl font-semibold text-[#343a40]">{title}</h2>
             <div class="text-md text-[#343a40]">
                 <p>{description}</p>
@@ -16,7 +16,7 @@
         </div>
     </div>
     <div class="px-10">
-        <div class=" w-[600px] shadow-xl rounded-2xl overflow-hidden h-auto">
+        <div data-aos="flip-left" data-aos-delay={150} class=" w-[600px] shadow-xl rounded-2xl overflow-hidden h-auto">
             <img src={image} alt="" class="w-full h-full object-cover" />
         </div>
     </div>
