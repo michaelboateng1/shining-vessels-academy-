05703231132e036df279302263ed8e01ab8d5d53
Michael Boateng <michaelboateng064@gmail.com>
2025-10-23 12:40:03 +0000

    Added admission page

---
+++ src/routes/admission/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Heading from "../components/Heading.svelte";
+    import AboutInfo from "../components/AboutInfo.svelte";
+
+    const message = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui dolorum itaque iusto quasi in molestiae dolores, ullam expedita? Hic, ullam sint. Maxime tenetur quibusdam aspernatur eos id corporis commodi animi!";
+</script>
+
+<main>
+    <Heading {message}>
+        <h1 class="text-5xl font-bold mb-4" data-aos="fade-in"><span class="text-[#fb8961]">SVA</span> Admissions</h1>
+    </Heading>
+    <AboutInfo />
+</main>
+
