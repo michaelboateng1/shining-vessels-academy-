28b695c456030fd084a0e3e8605cd3d22f0f2933
Michael Boateng <michaelboateng064@gmail.com>
2025-10-23 12:40:39 +0000

    Added a calender page

---
+++ src/routes/calender/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Calender from "../components/Calender.svelte";
+    import Heading from "../components/Heading.svelte";
+</script>
+
+<main>
+    <Heading />
+    <Calender />
+</main>
