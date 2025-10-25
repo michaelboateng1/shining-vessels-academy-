32e7aa453d116e3c975af5d11e8af5a55659de40
Michael Boateng <michaelboateng064@gmail.com>
2025-10-25 12:35:49 +0000

    Changed Students Life and added School Activities

---
+++ src/routes/school-activities/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import SchoolActivities from "../components/SchoolActivities.svelte";
+	import SchoolActivitiesCard from "../components/SchoolActivitiesCard.svelte";
+</script>
+
+<main>
+    <SchoolActivities />
+</main>
