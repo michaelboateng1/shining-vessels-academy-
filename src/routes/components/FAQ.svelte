daeac2fdcea82e2c7603055edd270470d1a0a1d8
Michael Boateng <michaelboateng064@gmail.com>
2025-10-26 00:20:12 +0000

    Added FAQs

---
+++ src/routes/components/FAQ.svelte
@@ -0,0 +1,44 @@
+<script>
+	const { faqs } = $props();
+
+	let current = $state(null);
+
+	const toggleFaq = (i) => {
+		current = i !== current ? i : null;
+	};
+</script>
+
+<section class="mt-20">
+	<h2 class="customShadow mb-[3rem] text-center text-[2.5rem] font-bold text-[#04095d] capitalize">
+		Questions & Answers
+	</h2>
+
+	<div class="container mx-auto grid grid-cols-2 gap-8" data-aos="fade-up" data-aos-delay={200}>
+		{#each faqs as faq, i}
+			<div class={i === current ? 'rounded-lg shadow-sm' : ''}>
+				<div
+					onclick={() => toggleFaq(i)}
+					class=" rounded-t-lg border-l-4 border-[#ccc] bg-[#04095d] p-5 text-lg text-white transition-all duration-100 {i !==
+					current
+						? 'rounded-lg shadow-sm'
+						: ''}"
+				>
+					<h1>{faq.question}</h1>
+				</div>
+				<p
+					class=" max-h-0 bg-[#ccc] px-10 text-[#04095d] transition-all duration-300 {i === current
+						? 'max-h-full rounded-b-lg py-5'
+						: 'max-h-0 overflow-hidden py-0 '}"
+				>
+					{faq.answer}
+				</p>
+			</div>
+		{/each}
+	</div>
+</section>
+
+<style>
+	.customShadow {
+		text-shadow: 1px 1px 2px #fb8961;
+	}
+</style>
---
+++ src/routes/faq/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+	import Heading from '../components/Heading.svelte';
+	import FAQ from '../components/FAQ.svelte';
+
+	const faqs = [
+		{
+			question: 'Lorem, ipsum.',
+			answer:
+				'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem itaque cumque officia provident ratione est laborum, odit fugiat atque ad.'
+		},
+		{
+			question: 'Lorem, ipsum.',
+			answer:
+				'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem itaque cumque officia provident ratione est laborum, odit fugiat atque ad.'
+		},
+		{
+			question: 'Lorem, ipsum.',
+			answer:
+				'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem itaque cumque officia provident ratione est laborum, odit fugiat atque ad.'
+		}
+	];
+
+	const message =
+		'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit, commodi.';
+</script>
+
+<main>
+	<Heading {message}>
+		<h1 class="mb-4 text-5xl font-bold" data-aos="fade-in">
+			Questions About <span class="text-[#fb8961]">SAV</span>
+		</h1>
+	</Heading>
+	<FAQ {faqs} />
+</main>
