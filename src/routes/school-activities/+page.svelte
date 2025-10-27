1408a8c2fd819aa6791932b8b1a57a69d551aba3
Michael Boateng <michaelboateng064@gmail.com>
2025-10-27 10:24:34 +0000

    Added changed school-activity page content

--- src/routes/school-activities/+page.svelte
+++ src/routes/school-activities/+page.svelte
@@ -1,8 +1,15 @@
 <script>
-    import SchoolActivities from "../components/SchoolActivities.svelte";
-	import SchoolActivitiesCard from "../components/SchoolActivitiesCard.svelte";
+    import WhoWeAre from "../components/WhoWeAre.svelte";
+    import Heading from "../components/Heading.svelte";
+    import WhereYouBelong from "../components/WhereYouBelong.svelte";
+
+    const message = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum atque quisquam voluptatibus possimus illo, autem minus earum eligendi nulla veritatis.";
 </script>
 
 <main>
-    <SchoolActivities />
+    <Heading {message}>
+        <h1 class="text-5xl font-bold mb-4" data-aos="fade-in"><span class="text-[#fb8961]">SVA</span> Activities</h1>
+    </Heading>
+    <WhoWeAre />
+    <WhereYouBelong />
 </main>
