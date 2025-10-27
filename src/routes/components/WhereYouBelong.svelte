d28cf3c58ed7bf3669ff38fc40db214e63f05961
Michael Boateng <michaelboateng064@gmail.com>
2025-10-27 10:22:32 +0000

    Added WhereYouBelong and WhoWeAre components to the School-activities page

---
+++ src/routes/components/WhereYouBelong.svelte
@@ -0,0 +1,40 @@
+<script>
+    import image1 from "$lib/assets/images/entertaimment4.jpg"; 
+    import image2 from "$lib/assets/images/competetion5.jpg"; 
+</script>
+
+<section class="container mx-auto mt-20 px-20">
+    <div class="px-5 space-y-4 text-[#343a40]">
+        <h2 class="text-[#04095d] font-semibold text-xl mb-2">Where you belong.</h2>
+        <p class="text-[#04095d] font-bold mb-5">Lorem, ipsum. Lorem, ipsum.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum temporibus dolor ullam, dolores quos, voluptates et nam molestiae porro dolorum ducimus soluta tempore? Rem quia vitae minima laboriosam. Temporibus, dignissimos repudiandae ab possimus illo nobis dolore nesciunt rerum sint recusandae?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, possimus!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla facere modi blanditiis, explicabo cumque molestiae nostrum nemo velit laborum deserunt!</p>
+    </div>
+    <div class="flex gap-5 w-full mt-8 h-[350px]">
+        <div class="w-full h-full">
+            <figure class="relative">
+                <img src={image1} alt="Entertainment" class="w-full h-full object-cover" />
+                <div class="absolute w-full h-full top-0 left-0"></div>
+                <figcaption class="absolute left-1/2 transform -translate-x-1/2 bottom-10 px-20 py-2 text-white font-bold italic border-r-4 border-l-4 border-[#fb8961] text-nowrap">SVA Teachers</figcaption>
+            </figure>
+        </div>
+        <div class="w-full h-full">
+            <figure class="relative">
+                <img src={image2} alt="Competetion" class="w-full h-full object-cover" />
+                <div class="absolute w-full h-full top-0 left-0"></div>
+                <figcaption class="absolute left-1/2 transform -translate-x-1/2 bottom-10 px-20 py-2 text-white font-bold italic border-r-4 border-l-4 border-[#fb8961] text-nowrap">SVA Graduates</figcaption>
+            </figure>
+        </div>
+    </div>
+</section>
+
+<style>
+    img +div{
+        background: linear-gradient(hsla(237, 92%, 19%, .0) 70%, hsla(237, 92%, 19%, .5));
+    }
+
+    figcaption{
+        background-color: hsla(237, 92%, 19%, .5);
+    }
+</style>
---
+++ src/routes/components/WhoWeAre.svelte
@@ -0,0 +1,48 @@
+<script>
+    import schoolVideo from "$lib/assets/videos/CADET DISPLAY - SVA CADET GROUP.mp4";
+	import { onMount } from "svelte";
+
+    let videoContainer;
+    let playBtn;
+
+    onMount(() => {
+        const video = document.createElement("video");
+        video.src = schoolVideo;
+        video.classList = "w-full h-full object-cover";
+        videoContainer.append(video);
+
+        playBtn.append("");
+        playBtn.addEventListener("click", (e) => {
+            video.play()
+            video.controls = true;
+            playBtn.style.visibility = "hidden"
+        })
+
+        video.onpause = () => {
+                playBtn.style.visibility = "visible"
+                video.controls = false
+            }
+    })
+</script>
+
+<section class="mt-20 container mx-auto">
+    <h2 class="customShadow mb-[3rem] text-center text-[2.5rem] font-bold text-[#04095d] capitalize">Who we are</h2>
+    <!-- TODO:replace the image with a youtube video -->
+    <div class="w-[600px] h-auto relative mx-auto border-l-8 border-r-8 border-[#04095d] rounded-lg">
+        <div class="w-full h-auto" bind:this={videoContainer} ></div>
+        <div bind:this={playBtn} class="w-[120px] transition-all duration-300 cursor-pointer flex justify-center items-center h-[120px] rounded-full border-4 border-white absolute top-1/2 left-1/2 transform -translate-1/2">
+            <div class="w-[40px] h-[40px] bg-white play"></div>
+        </div>
+    </div>
+</section>
+
+<style>
+    .customShadow {
+		text-shadow: 1px 1px 2px #fb8961;
+	}
+
+    .play{
+        clip-path: polygon(0% 0%, 100% 50%, 0% 100%, 0% 0%, 0% 0%, 0% 0%);
+        cursor: pointer;
+    }
+</style>
